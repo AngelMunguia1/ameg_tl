e06b1458537eba3140f0d8f5bfec499064deb32a
OscarFernandoMillanPimentel <o_fernando_m_p@ciencias.unam.mx>
2022-05-25 14:57:43 -0500

    convobackend

--- backend/App/views/convocatoria_all.php
+++ backend/App/views/convocatoria_all.php
@@ -1,56 +1,4 @@
-<!DOCTYPE html>
-<html lang="zxx">
-	<head>
-
-		<!-- Basic -->
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-		<title>Sistema de trabajo libres de AMEG</title>	
-
-		<meta name="keywords" content="HTML5 Template" />
-		<meta name="description" content="EZY - Responsive HTML5 Template">
-		<meta name="author" content="okler.net">
-
-		<!-- Mobile Metas -->
-		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
-
-		<!-- Web Fonts  -->
-		<link href="https://fonts.googleapis.com/css?family=Montserrat:100,300,400,500,600,700,900%7COpen+Sans:300,400,600,700,800" rel="stylesheet" type="text/css">
-
-		<!-- Vendor CSS -->
-		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
-		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.css">
-		<link rel="stylesheet" href="vendor/fontawesome-free/css/all.min.css">
-		<link rel="stylesheet" href="vendor/fontawesome-free/css/all.css">
-		<link rel="stylesheet" href="vendor/animate/animate.min.css">
-		<link rel="stylesheet" href="vendor/linear-icons/css/linear-icons.min.css">
-		<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.carousel.min.css">
-		<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.theme.default.min.css">
-		<link rel="stylesheet" href="vendor/magnific-popup/magnific-popup.min.css">
-		<link rel="stylesheet" href="master/style-switcher/bootstrap-colorpicker/css/bootstrap-colorpicker.css">
-
-
-
-		<!-- Theme CSS -->
-		<link rel="stylesheet" href="css/theme.css">
-		<link rel="stylesheet" href="css/theme-elements.css">
-
-		<!-- Current Page CSS -->
-		<link rel="stylesheet" href="vendor/rs-plugin/css/settings.css">
-		<link rel="stylesheet" href="vendor/rs-plugin/css/layers.css">
-		<link rel="stylesheet" href="vendor/rs-plugin/css/navigation.css">
-
-		<!-- Skin CSS -->
-		<link rel="stylesheet" href="css/skins/default.css"> 
-
-		<!-- Theme Custom CSS -->
-		<link rel="stylesheet" href="css/custom.css">
-
-		<!-- Head Libs -->
-		<script src="vendor/modernizr/modernizr.min.js"></script>
-
-	</head>
+<?php echo $header; ?>
 	<body>
 		<div class="body">
 
@@ -230,65 +178,64 @@
 				</section> -->
 				<hr class="my-0">
 			</div>
- 
-			<footer id="footer" class="mt-0">
-				<div class="footer-copyright bg-light mt-0">
-					<div class="container mt-3">
-						<div class="row text-center">
-							<div class="col">
-								<p>GRUPO LAHE. © 2022. All Rights Reserved</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</footer>
+
 		</div>
-
-		<!-- Vendor -->
-		<script src="vendor/jquery/jquery.min.js"></script>
-		<script src="vendor/jquery.appear/jquery.appear.min.js"></script>
-		<script src="vendor/jquery.easing/jquery.easing.min.js"></script>
-		<script src="vendor/jquery.cookie/jquery.cookie.js"></script>
-		<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-		<script src="vendor/common/common.min.js"></script>
-		<script src="vendor/jquery.validation/jquery.validate.min.js"></script>
-		<script src="vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
-		<script src="vendor/jquery.gmap/jquery.gmap.min.js"></script>
-		<script src="vendor/jquery.lazyload/jquery.lazyload.min.js"></script>
-		<script src="vendor/isotope/jquery.isotope.min.js"></script>
-		<script src="vendor/owl.carousel/owl.carousel.min.js"></script>
-		<script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
-		<script src="vendor/vide/jquery.vide.min.js"></script>
-		<script src="vendor/vivus/vivus.min.js"></script>
-		<script src="master/style-switcher/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js"></script>
-		
-		<!-- Theme Base, Components and Settings -->
-		<script src="js/theme.js"></script>
-		
-		<!-- Current Page Vendor and Views -->
-		<script src="vendor/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
-		<script src="vendor/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
-		
-		<!-- Theme Custom -->
-		<script src="js/custom.js"></script>
-		
-		<!-- Theme Initialization Files -->
-		<script async src="js/theme.init.js"></script>
-		
-		<!-- Examples -->
-		<script src="js/examples/examples.landing.js"></script>
-
-		<!-- Google Analytics: Change UA-XXXXX-X to be your site's ID. Go to http://www.google.com/analytics/ for more information.
-		<script>
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		
-			ga('create', 'UA-12345678-1', 'auto');
-			ga('send', 'pageview');
-		</script>
-		 -->
 
 	</body>
 </html>
+
+    <script>
+        $( document ).ready(function() {
+
+            $("#form_paymet_ticket").on("submit",function(event){
+                event.preventDefault();
+                
+                    var formData = new FormData(document.getElementById("form_paymet_ticket"));
+                    console.log(formData);
+                    $.ajax({
+                    url:"/Home/uploadTicket",
+                    type: "POST",
+                    data: formData,
+                    cache: false,
+                    contentType: false,
+                    processData: false,
+                    beforeSend: function(){
+                        console.log("Procesando....");
+
+                    
+                    },
+                    success: function(respuesta){
+                        
+                        
+                        if(respuesta == 'success'){
+                           // $('#modal_payment_ticket').modal('toggle');
+                            Swal.fire(
+                                'OK',
+                                'Se ha guardado tu ticket correctamente!!',
+                                'success'
+                            )
+
+                            setTimeout(() => {
+                                window.location.reload();
+                            }, 100);
+                            
+                            
+                        }
+                        
+                        console.log(respuesta);
+
+                    },
+                    error:function (respuesta)
+                    {
+                        console.log(respuesta);
+                    }
+
+                });
+            });
+
+        });
+         
+    
+    </script>
+
+<?php echo $footer; ?>
