d507f6a301a88addbbd8753eea4312a485562a98
OscarFernandoMillanPimentel <o_fernando_m_p@ciencias.unam.mx>
2022-06-03 09:10:40 -0500

    textsize

--- backend/App/views/inicio_all.php
+++ backend/App/views/inicio_all.php
@@ -88,15 +88,15 @@
 					<br><br>
 					<div class="container container-lg-custom align-content-center pb-lg-5 mb-lg-4">
 						<div class="row align-items-center">
-						<div class="col-lg-3">
-								<a id="linkIngreso" href="/Instrucciones/?trabajo=trabajos_ingreso">
-								<div class="icon-box-info text-center ameg-shadow-box-btn">
-									<h2 class="font-weight-bold text-center text-color-dark text-4 mb-3">Trabajos de ingreso</h2>
-									<img width="70" src="/vendor/iconos/mesa1.png"/>
-								</div>
-								</a>
-							
-						</div>
+							<div class="col-lg-3">
+									<a id="linkIngreso" href="/Instrucciones/?trabajo=trabajos_ingreso">
+									<div class="icon-box-info text-center ameg-shadow-box-btn">
+										<h2 class="font-weight-bold text-center text-color-dark text-3 mb-3">Trabajos de ingreso</h2>
+										<img width="70" src="/vendor/iconos/mesa1.png"/>
+									</div>
+									</a>
+								
+							</div>
 							<div class="col-lg-3">
 									<a id="linkIngreso" href="/Instrucciones/?trabajo=trabajos_orales">
 									<div class="icon-box-info text-center ameg-shadow-box-btn">
